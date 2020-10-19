0e25e4783167dc9567b6e0cf5f39c2e6c4415337
Goncalo Santos Revesso Matos Pereira <a92973@iscte.pt>
2020-10-19 15:24:59 +0100

    criar classe funcional para pacientes

---
+++ cria_pacientes.sh
@@ -0,0 +1,8 @@
+> pacientes.txt
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+    numero=$(cat /etc/passwd | tail +56 | head -10 | awk -F "[:]" '{print $1}' | sed -E "s/a//g" | head -$i | tail -1)
+    nome=$(cat /etc/passwd | tail +56 | head -10 | awk -F "[:]" '{print $5}' | sed -E "s/,//g" | head -$i | tail -1)
+    email=$(cat /etc/passwd | tail +56 | head -10 | awk -F "[:]" '{print $1}' | head -$i | tail -1)"@iscte-iul.pt"
+    echo "$numero;$nome;;;$email;100" >> pacientes.txt
+done 
